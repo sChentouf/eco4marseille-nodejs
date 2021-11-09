9b7189f5cec9990fb9db11d962a16ec3ab49ab07
Siham <siham.chentouf@epitech.eu>
2021-11-09 11:07:49 +0100

    ajout de connxion bdd

--- project/index.js
+++ project/index.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//app.use(express.static(path.join(__dirname, "./index.html")));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.use("/element", express.static(__dirname + "/element"));
+
+const db = mysql.createConnection({
+  database: "db",
+  host: "localhost",
+  user: "user",
+  password: "password",
+});
+
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Connecté à la base de données MySQL!");
+});
